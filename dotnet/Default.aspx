6a4580577622e84796878836a04d9e0406d4933f
TuoKy <tuomas.kytta@gmail.com>
2015-10-22 15:44:26 +0300

    Tunnin räpellykset

---
+++ dotnet/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Heippa maailma</h1>
+        <asp:Label ID="lblHello" runat="server" Text="label"> Yo, anna nimi</asp:Label>
+        <asp:TextBox ID="tebNimi" runat="server"></asp:TextBox>
+        <asp:Button ID="btnFire" Text="paina Tästä"  runat ="server" OnClick="btnFire_Click"/>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ dotnet/DemoxOy.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class DemoxOy : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Ladataan jutut tietokannast
+        getRealData();
+    }
+    protected void getDemonData()
+    {
+        //täytetään gridview jollain
+        DataTable dt = JAMK.IT.DBDemoxOy.GetDataSimple();
+        gvCustomers.DataSource = dt;
+        gvCustomers.DataBind();
+    }
+
+    protected void getRealData()
+    {
+        DataTable dt = JAMK.IT.DBDemoxOy.GetDataReal();
+        gvCustomers.DataSource = dt;
+        gvCustomers.DataBind();
+    }
+}
